93ffa4d809c51cf3dd2759e19da9df76bf07d069
mikaelmendoza21 <mikaelmendoza21@gmail.com>
2018-03-21 21:17:21 -0700

    Basic file server implementation. Includes the 'fileSettings' module, with basic file configurations
    Signed-off-by: mikaelmendoza21 <mikaelmendoza21@gmail.com>

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+var mimeTypes = require('./fileSettings');
+
+const port = process.argv[2] || 9000;
+
+http.createServer(function (req, res) {
+  console.log(`${req.method} ${req.url}`);
+  // parse URL
+  const parsedUrl = url.parse(req.url);
+  // extract URL path
+  let pathname = `.${parsedUrl.pathname}`;
+
+  fs.exists(pathname, function (exist) {
+    if(!exist) {
+      // if the file is not found, return 404
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+    // if is a directory, then look for index.html
+    if (fs.statSync(pathname).isDirectory()) {
+      pathname += '/index.html';
+    }
+    
+    // Read from filesystem
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } 
+      else {
+        // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+        const ext = path.parse(pathname).ext;
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', mimeTypes[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
+}).listen(parseInt(port));
+console.log(`neatoNode Server listening on port ${port}`);
